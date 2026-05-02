54f9f79f6aa8150256accd4104d861c4ff68dba4
juanhincapieh <juanhincapieh@icloud.com>
2026-05-02 09:01:10 -0500

    feat: migrated DarkVeil and Navbar to app file in order to have them on layout

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,28 @@
 <template>
-  <div class="min-h-screen bg-neutral-950">
-    <NuxtRouteAnnouncer />
-    <NuxtPage />
+  <div class="relative min-h-screen bg-neutral-950">
+    <div class="fixed inset-0 z-0 opacity-50">
+        <DarkVeil
+            :hue-shift="0"
+            :noise-intensity="0"
+            :scanline-intensity="0"
+            :speed="2"
+            :scanline-frequency="0"
+            :warp-amount="0"
+            :resolution-scale="1"
+        />
+    </div>
+    <div class="relative z-10 flex flex-col max-w-7xl mx-auto w-full px-6 md:px-12 pt-4">
+        <Navbar />
+        <NuxtRouteAnnouncer />
+        <NuxtPage />
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+// UIKit
+import Navbar from '@/components/Navbar.vue';
+
+// VueBits
+import DarkVeil from '@/components/VueBits/Backgrounds/DarkVeil.vue';
+</script>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,27 +1,11 @@
 <template>
     <div class="relative min-h-screen">
-        <div class="absolute inset-0 z-0 opacity-50">
-            <DarkVeil 
-                :hue-shift="0"
-                :noise-intensity="0"
-                :scanline-intensity="0"
-                :speed="0.7"
-                :scanline-frequency="0"
-                :warp-amount="0"
-                :resolution-scale="1"
-            />
-        </div>
-        <div class="relative flex flex-col  gap-16 max-w-7xl mx-auto w-full px-6 md:px-12 pt-10 z-10">
-            <Navbar />
+        <div class="relative flex flex-col gap-16 max-w-7xl mx-auto mt-14 w-full z-10">
             <Hero />
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
-// UIKit
-import Navbar from '@/components/Navbar.vue';
-
-// VueBits
-import DarkVeil from '@/components/VueBits/Backgrounds/DarkVeil.vue';
+import Hero from '@/components/Hero.vue';
 </script>
