5c5ee0a20570a43b62cf36835e2649be505f22ac
juanhincapieh <juanhincapieh@icloud.com>
2026-05-01 22:22:30 -0500

    feat: imported NuxtPage into app.vue file and added main class for layout

---
+++ app/pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="flex flex-col px-80 pt-12">
+        <Navbar />
+    </div>
+</template>
+
+<script setup lang="ts">
+import Navbar from '@/components/Navbar.vue';
+</script>
