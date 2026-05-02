839002eb0151abba69c66b78b6c98509604c21ec
juanhincapieh <juanhincapieh@icloud.com>
2026-05-02 00:22:31 -0500

    feat: added DarkVeil background to index

--- app/components/Navbar.vue
+++ app/components/Navbar.vue
@@ -1,32 +1,32 @@
 <template>
     <div class="mt-3">
-        <div class="flex items-center align-middle justify-between mx-2">
-            <h1 class="text-white text-xl font-bold">juanhincapieh</h1>
+        <div class="flex items-center align-middle justify-between mx-2 bg-white/10 backdrop-blur-3xl py-3 px-4 rounded-2xl">
+            <h1 class="text-white text-xl font-semibold">juanhincapieh</h1>
             <div class="flex align-middle items-center space-x-8">
                 <NuxtLink 
                     to="/" 
-                    class="flex flex-col items-center text-gray-400/80 hover:text-zinc-400"
+                    class="flex flex-col items-center text-gray-300/80 hover:text-zinc-400"
                     :class="{ 'text-white' : route.path === '/' }"
                 >
                     <Home :size="30" />
                 </NuxtLink>
                 <NuxtLink 
                     to="/about" 
-                    class="flex flex-col items-center text-gray-400/80 hover:text-zinc-400"
+                    class="flex flex-col items-center text-gray-300/80 hover:text-gray-400"
                     :class="{ 'text-white' : route.path === '/about' }"
                 >
                     <UserRound :size="30" />
                 </NuxtLink>
                 <NuxtLink 
                     to="/projects" 
-                    class="flex flex-col items-center text-gray-400/80 hover:text-zinc-400"
+                    class="flex flex-col items-center text-gray-300/80 hover:text-gray-400"
                     :class="{ 'text-white' : route.path === '/projects' }"
                 >
                     <FolderRoot :size="30" />
                 </NuxtLink>
                 <NuxtLink 
                     to="/contact" 
-                    class="flex flex-col items-center text-gray-400/80 hover:text-zinc-400"
+                    class="flex flex-col items-center text-gray-300/80 hover:text-gray-400"
                     :class="{ 'text-white' : route.path === '/contact' }"
                 >
                     <Mail :size="30" />
@@ -37,7 +37,7 @@
                     :size="'md'"
                     :variant="'default'"    
                     :icon="IdCardLanyard"
-                    @click="router.push({ path: '/files/cv.pdf' })"
+                    @click=""
                 >
                     Download CV
                 </Button>
@@ -60,5 +60,4 @@ import {
 } from '@lucide/vue';
 
 const route = useRoute();
-const router = useRouter();
 </script>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,9 +1,26 @@
 <template>
-    <div class="flex flex-col px-80 pt-12">
-        <Navbar />
+    <div class="relative min-h-screen">
+        <div class="absolute inset-0 z-0 opacity-50">
+            <DarkVeil 
+                :hue-shift="40"
+                :noise-intensity="0"
+                :scanline-intensity="0"
+                :speed="0.7"
+                :scanline-frequency="0"
+                :warp-amount="0"
+                :resolution-scale="1"
+            />
+        </div>
+        <div class="relative flex flex-col max-w-7xl mx-auto w-full px-6 md:px-12 pt-12 z-10">
+            <Navbar />
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
+// UIKit
 import Navbar from '@/components/Navbar.vue';
+
+// VueBits
+import DarkVeil from '@/components/VueBits/Backgrounds/DarkVeil.vue';
 </script>
